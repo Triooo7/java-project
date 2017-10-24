b1c806bf08ad2dda3a6be1d6d5ce6d07554b3f2d
rohithraju <krishna.raju93@gmail.com>
2017-10-24 13:28:01 +0000

    some init msg

---
+++ Rectangulator.java
@@ -0,0 +1,21 @@
+	public class Rectangluator {
+	public static void main (String[] args) {
+
+		int length = Integer.parseInt(args[0]);
+		int width = Integer.parseInt(args[1]);
+
+
+		Rectangle myRectangle = new Rectangle (length, width);
+
+		String output = String.format("********** \n L: %d\n W: %d \n A:		%d \n P: , myRectangle.length, myRectangle.width, myRectangle.getArea(), myRectangle.getPerimeter());
+
+	System.out.println(output);
+
+
+}
+
+
+
+
+
+}
